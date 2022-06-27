d8b5ec10bc07345a9c688ba540dae3aa1c9d0581
procopios <enprocopios@gmail.com>
2022-06-27 07:18:31 -0700

    Initialize 06_PATCH_DELETE

---
+++ 06_PATCH_DELETE/project_showcase/src/App.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import ProjectList from "./components/ProjectList";
+import ProjectEditForm from "./components/ProjectEditForm";
+
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [projects, setProjects] = useState([]);
+  const [projectId, setProjectId] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/projects")
+      .then((resp) => resp.json())
+      .then((projects) => setProjects(projects));
+  });
+
+  const onToggleDarkMode = () => {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  };
+
+  const onAddProject = (newProj) => {
+    setProjects((projects) => [...projects, newProj]);
+  };
+
+  const completeEditing = () => {
+    setProjectId(null);
+  };
+
+  const enterProjectEditModeFor = (projectId) => {
+    setProjectId(projectId);
+  };
+
+  const renderForm = () => {
+    if (projectId) {
+      return (
+        <ProjectEditForm
+          projectId={projectId}
+          completeEditing={completeEditing}
+        />
+      );
+    } else {
+      return <ProjectForm onAddProject={onAddProject} />;
+    }
+  };
+
+  return (
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
+      {renderForm()}
+      <ProjectList
+        projects={projects}
+        enterProjectEditModeFor={enterProjectEditModeFor}
+      />
+    </div>
+  );
+};
+
+export default App;
