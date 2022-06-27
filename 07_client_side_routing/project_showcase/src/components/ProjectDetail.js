b2cb5254503021c35b6089a5bf219140d2dcb581
procopios <enprocopios@gmail.com>
2022-06-27 07:18:31 -0700

    Initialize 07_client_side_routing

---
+++ 07_client_side_routing/project_showcase/src/components/ProjectDetail.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+
+const ProjectDetail = () => {
+  const [claps, setClaps] = useState(0);
+  const [project, setProject] = useState(null);
+
+  const id = 1;
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/projects/${id}`)
+      .then((r) => r.json())
+      .then((project) => {
+        setProject(project);
+      });
+  }, [id]);
+
+  const { image, name, about, link, phase } = project;
+
+  const handleClapClick = () => {
+    setClaps((claps) => claps + 1);
+  }
+
+  return (
+    <section>
+      <div className="project-detail box">
+        <div className="project-image">
+          <img src={image} alt={name} />
+          <button className="claps" onClick={handleClapClick}>
+            👏{claps}
+          </button>
+        </div>
+        <div className="details">
+          <h2>{name}</h2>
+          <p>{about}</p>
+          {link ? (
+            <p>
+              <a target="_blank" rel="noreferrer" href={link}>
+                Project Homepage
+              </a>
+            </p>
+          ) : null}
+          <div className="extra">
+            <span className="badge blue">Phase {phase}</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectDetail;
