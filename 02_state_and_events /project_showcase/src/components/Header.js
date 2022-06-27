2398e72f159e13b1a615e769f7966455646d806c
procopios <enprocopios@gmail.com>
2022-06-27 07:18:27 -0700

    Initialize 02_state_and_events

---
+++ 02_state_and_events /project_showcase/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <span className="logo">{"//"}</span>
+        Project Showcase
+      </h1>
+      <button>Dark Mode</button>
+    </header>
+  );
+}
+
+export default Header;
