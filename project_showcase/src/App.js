1e8cac709cb3f235fdf599b6842c5b615e8385c4
procopios <enprocopios@gmail.com>
2022-06-27 07:18:26 -0700

    Initialize components_and_props_solution

---
+++ project_showcase/src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import ProjectList from "./components/ProjectList";
+
+import projects from "./projects";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <ProjectForm />
+      <ProjectList projects={projects} />
+    </div>
+  );
+};
+
+export default App;
