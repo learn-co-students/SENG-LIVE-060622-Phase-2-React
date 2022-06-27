c402397b276d76123c78d3b52349ea01096f35a4
procopios <enprocopios@gmail.com>
2022-06-27 07:18:29 -0700

    Initialize react_forms_solution

---
+++ project_showcase/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import ProjectForm from "./components/ProjectForm";
+import ProjectList from "./components/ProjectList";
+
+const App = () => {
+  const [projects, setProjects] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleClick = () => {
+    fetch("http://localhost:4000/projects")
+      .then((res) => res.json())
+      .then((projects) => setProjects(projects));
+  };
+
+  const onToggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
+  const onAddProject = (newProject) => {
+    setProjects([...projects, newProject]);
+  };
+
+  return (
+    <div className={isDarkMode ? "App" : "App light"}>
+      <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
+      <ProjectForm onAddProject={onAddProject} />
+      <button onClick={handleClick}>Load Projects</button>
+      <ProjectList projects={projects} />
+    </div>
+  );
+};
+
+export default App;
